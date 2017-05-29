2d6f1c04f8a59e712437f95f9d9c58cc4db62eb0
Luuk Nieuwdorp <luuknieuwdorp@gmail.com>
2017-05-29 22:11:28 +0200

    Fix icon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = require('electron')
 
 app.on('ready', () => {
   var fs = require('fs');
-  let win = new BrowserWindow({width:800, height:600, icon: __dirname + '/icon.png'})
+  let win = new BrowserWindow({width:800, height:600, icon: __dirname + '/icons/icon.png'})
   win.loadURL('https://web.whatsapp.com')
 //win.setMenu(null);
   let content = win.webContents
